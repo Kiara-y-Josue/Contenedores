ef7223b27d7683bf5abcb9c99cd107815b173468
unknown <erickmoran153@gmail.com>
2024-06-17 10:21:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationconten/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplicationconten;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        String[] datos = new String[]{"Primero", "Segundo", "Tercero", "Cuarto", "Quinto", "Sexto", "Septimo", "Octavo", "Noveno", "Decimo"};
+        ArrayAdapter<String> adapter = new
+                ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, datos);
+        AutoCompleteTextView textView =
+                findViewById(R.id.txtcontainer);
+        textView.setAdapter(adapter);
+    }
+}
